002c5bd6d8f089de8e134987ee1d07074c28cbbc
sabaif-s <sebaifmuhammed33@gmail.com>
2024-12-22 12:57:20 +0300

    adding metadata

---
+++ app/metadata/page.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function  Metadata() {
+  return (
+    <div> Metadata</div>
+  )
+}
+
+export default  Metadata
+export const metadata = {
+    title: 'Meta Data Route',
+    description: 'This is Metadata',
+  };
+  
